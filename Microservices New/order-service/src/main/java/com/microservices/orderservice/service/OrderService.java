a061cbf1cf94339203485f4152c5969dbfc3fd1b
Rahul Vijay <105475681+RahulVijay17@users.noreply.github.com>
2023-07-01 16:00:33 +0530

    Delete Microservices New directory

--- Microservices New/order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.orderservice.service;
-
-import com.microservices.orderservice.model.OrderRequest;
-
-public interface OrderService {
-    Long placeOrder(OrderRequest orderRequest);
-}
